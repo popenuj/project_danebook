2f9f874e6852e29ca86d1a770458bb1291d855f6
Jonathan Popenuck <john.popenuck@gmail.com>
2016-12-16 12:00:59 -0800

    tweaked photo show page and added photos index to nav

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,13 +3,11 @@ class Photo < ApplicationRecord
   belongs_to        :user
 
   has_attached_file :photo,
-              styles: { thumb: "100x100" }
-    #  convert_options: { thumb: '-strip
-    #                             -quality 30%
-    #                             -resize 600x
-    #                             -sharpen 0x0.5
-    #                             source.jpg
-    #                             output.jpg' }
+              styles: { thumb: "100x100" },
+     convert_options: { thumb: '-strip
+                                -quality 30%
+                                -resize 600x
+                                -sharpen 0x0.5'}
 
     validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
