5737fa4ba542dd7e44bf2fe366d4abc2afaa2805
Jonathan Popenuck <john.popenuck@gmail.com>
2016-12-19 16:32:23 -0800

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+GENDER = ["male", "female"]
+puts "Seeding users"
+20.times do
+  User.create(email: Faker::Internet.email,
+              password: "password1",
+              password_confirmation: "password1")
+end
+
+puts "Seeding profiles"
+User.all.each do |user|
+  user.profile(user_id: user.id,
+                   first_name: Faker::Name.first_name,
+                    last_name: Faker::Name.last_name,
+                     birthday: Faker::Date.backward(1000),
+                       gender: GENDER.sample,
+                      college: Faker::Educator.university,
+                     hometown: Faker::Address.city,
+              currently_lives: Faker::Address.city,
+                    telephone: Faker::PhoneNumber.phone_number).save
+end
+
+puts "Seeding posts"
+60.times do
+  Post.create(user_id: User.all.sample.id,
+            post_text: Faker::StarWars.quote)
+end
+
+puts "Seeding comments"
+180.times do
+  Comment.create(user_id: User.all.sample.id,
+          commentable_id: Post.all.sample.id,
+        commentable_type: "post",
+            comment_text: Faker::Hipster.sentence)
+end
