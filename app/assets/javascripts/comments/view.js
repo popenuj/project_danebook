ee23838cc1cff5b9265bc7847493620f00f4d7bd
Jonathan Popenuck <john.popenuck@gmail.com>
2017-01-18 16:13:51 -0800

    finallllllllly fixed post styling

--- app/assets/javascripts/comments/view.js
+++ app/assets/javascripts/comments/view.js
@@ -3,9 +3,9 @@ DANEBOOK.Comments = DANEBOOK.Comments || {}
 DANEBOOK.Comments.View = (function($){
 
   var _addCommentToggleListener = function() {
-    $(".post").on("click", ".toggle-comment-form", function(e){
+    $(document).on("click", ".post .toggle-comment-form", function(e){
       $(e.target).next().slideDown(500);
-    })
+    });
   }
 
   var init = function() {
